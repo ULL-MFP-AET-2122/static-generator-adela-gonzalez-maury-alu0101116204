5e7d023af2d0c8928e5c4bec696ac2be987569c1
AdelaGM <alu0101116204@ull.edu.es>
2021-11-18 18:41:46 +0000

    new-deploy

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Aprendiendo a usar un Generador Estático",
+        "excerpt":"Por fin jueves   Hoy aparqué súper lejos y está lloviendo   Cogi mucha cola porque está lloviendo   Me aptece un té y una manta porque está lloviendo  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/aet",
+        "teaser": null
+      },{
+        "title": "Aprendiendo a hacer blogs",
+        "excerpt":"Martes 09 Noviembre 2021 Un día tranquilo Me gusta mucho comer naranjas Me gusta mucho comer limones Me gusta mucho comer papaya Me gusta mucho comer plátano un título Somos 20 alumnos Mi usuario de twitter es AdelaGM un título más pequeño Ay dios mío cuánto de semana queda… &lt;!–...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Clase   Clase del Jueves 11/11/2021   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase",
+        "teaser": null
+      },{
+        "title": "Tenis",
+        "excerpt":"Prueba tenis   Contar historia                                First name                Last name                Age                Location                              John            Doe            35            United States                           Jane            Doe            29            France                           Jack            Hill            25            Australia           ","categories": ["deporte"],
+        "tags": [],
+        "url": "/deporte/tenis",
+        "teaser": null
+      }]
---
+++ _site/assets/js/mathjax-configure.js
@@ -0,0 +1,8 @@
+window.MathJax = {
+tex: {
+    packages: ['base', 'ams']
+},
+loader: {
+    load: ['ui/menu', '[tex]/ams']
+}
+}
